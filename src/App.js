2a6c186ef6bf3612e0087576cd14f612cbcf1b1b
kaushik9859 <bhowmickkaushik1@gmail.com>
2024-11-08 20:47:14 +0530

    Update App.js
    M

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom"
+
+import Header from './components/Header';
+import HomeBody from './components/HomeBody';
+
 import Trackingpage from './pages/Tracking';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <HomeBody/>
       <Routes>
 
         <Route path='/tracking' element={<Trackingpage />}></Route>
