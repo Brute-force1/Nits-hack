cb942497114abf6f554eebcb788f7dd03fa0b4ee
Rahul Nath <155018123+Rahulnath76@users.noreply.github.com>
2024-11-08 22:21:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom"
 import Header from './components/Header';
+import Trackingpage from './pages/Tracking';
 import HomeBody from './components/HomeBody';
 import Trackingpage from './pages/Tracking';
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<HomeBody />} />
+        <Route path='/tracking' element={<Trackingpage />}></Route>
         <Route path='/tracking' element={<Trackingpage />}></Route>
       </Routes>
     </div>
