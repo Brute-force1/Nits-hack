a5b5259b95750256c000346766c1ddbb516544aa
Rahul Nath <155018123+Rahulnath76@users.noreply.github.com>
2024-11-08 21:20:50 +0530

    Update Tracking.js

--- src/pages/Tracking.js
+++ src/pages/Tracking.js
@@ -5,7 +5,7 @@ import kuvio from "../assests/Kuvio 1 Ruutuaika EN.svg"
 
 const Box = ({ headerText, text }) => {
   return (
-    <div className="box">
+    <div className="box1">
       <h4>{headerText}</h4>
       <p>{text}</p>
     </div>
@@ -17,23 +17,23 @@ const Trackingpage = () => {
     <>
         
         <div className="container1">
-      <h2 className="heading">Welcome Ella!</h2>
-      <p className="subheading">Your weekly summary</p>
+      <h2 className="heading1">Welcome Ella!</h2>
+      <p className="subheading1">Your weekly summary</p>
 
-      <div className="progress-container">
-        <div className='progress-bar-heading'>
-          <p className="progress-head">You’re on track to meet your goal!</p>
+      <div className="progress-container1">
+        <div className='progress-bar-heading1'>
+          <p className="progress-head1">You’re on track to meet your goal!</p>
 
-          <p className='progress-head'>6 hours</p>
+          <p className='progress-head1'>6 hours</p>
         </div>
-        <div className="progress-bar">
-          <div className="progress-fill"></div>
+        <div className="progress-bar1">
+          <div className="progress-fill1"></div>
         </div>
-        <p className="status-text">You have 15 hours left this week</p>
+        <p className="status-text1">You have 15 hours left this week</p>
       </div>
 
-      <div className="section">
-        <p className="section-heading">Your screen time each day</p>
+      <div className="section1">
+        <p className="section-heading1">Your screen time each day</p>
         {/* Screen time chart */}
         <div>
             <img src={kuvio} alt="" height={500}/>
@@ -41,25 +41,25 @@ const Trackingpage = () => {
         {/* <MyChartComponent /> */}
       </div>
 
-      <div className="section">
-        <h4 className="section-heading">Your goals</h4>
-        <div className="flex-container">
+      <div className="section1">
+        <h4 className="section-heading1">Your goals</h4>
+        <div className="flex-container1">
           <Box headerText="Limit daily goals " text="Time to wake up" />
           <Box headerText="Limit daily goals " text="Time to wake up" />
         </div>
         <div className="center-align">
-          <button className="button">Review and adjust goals</button>
+          <button className="button1">Review and adjust goals</button>
         </div>
       </div>
 
-      <div className="section">
-        <h4 className="section-heading">Your rewards</h4>
-        <div className="flex-container">
+      <div className="section1">
+        <h4 className="section-heading1">Your rewards</h4>
+        <div className="flex-container1">
           <Box headerText="Limit daily goals " text="Time to wake up" />
           <Box headerText="Limit daily goals " text="Time to wake up" />
         </div>
-        <div className="center-align">
-          <button className="button">View all rewards</button>
+        <div className="center-align1">
+          <button className="button1">View all rewards</button>
         </div>
       </div>
     </div>
